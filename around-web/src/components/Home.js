73fb033ae52830d3702bff16ca0ab3616938d27a
syx007 <suyuxuan007@hotmail.com>
2018-04-01 01:32:00 -0700

    add function to get geo location of user

--- around-web/src/components/Home.js
+++ around-web/src/components/Home.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import { Tabs, Button } from 'antd';
+import { GEO_OPTIONS } from "../constants"
+
+
 const TabPane = Tabs.TabPane;
 
 export class Home extends React.Component {
+    getGeoLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessGetGeoLocation,
+                this.onFailedGetGeoLocation,
+                GEO_OPTIONS);
+        } else {
+            /* geolocation IS NOT available */
+        }
+
+    }
+
+    onSuccessGetGeoLocation = (position) => {
+        console.log(position)
+    }
+
+    onFailedGetGeoLocation = () => {
+
+    }
+
+    componentDidMount() {
+        this.getGeoLocation();
+    }
     render() {
         const operations = <Button type='primary'>Create New Post</Button>;
         return (
