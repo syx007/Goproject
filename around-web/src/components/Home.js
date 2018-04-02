2f377e8511e38e4ffe80829290f8f6b4a72be258
syx007 <suyuxuan007@hotmail.com>
2018-04-02 15:15:14 -0700

    add loading icon in the main page

--- around-web/src/components/Home.js
+++ around-web/src/components/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Tabs, Button } from 'antd';
+import { Tabs, Button, Spin, Icon } from 'antd';
 import { GEO_OPTIONS } from "../constants"
 
 
 const TabPane = Tabs.TabPane;
+const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
 export class Home extends React.Component {
+    state  = {
+        loadingGeoLocation: false,
+    }
     getGeoLocation = () => {
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(
@@ -19,21 +23,28 @@ export class Home extends React.Component {
     }
 
     onSuccessGetGeoLocation = (position) => {
-        console.log(position)
+        this.setState({loadingGeoLocation: false});
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem('POS_KEY', JSON.stringify({lat : latitude, lon : longitude}))
     }
 
     onFailedGetGeoLocation = () => {
-
+        this.setState({loadingGeoLocation: false});
+        console.log('Failed to get geolocation')
     }
 
     componentDidMount() {
+        this.setState({loadingGeoLocation: true})
         this.getGeoLocation();
     }
     render() {
         const operations = <Button type='primary'>Create New Post</Button>;
         return (
             <Tabs className = 'main-tabs' tabBarExtraContent={operations}>
-                <TabPane tab="Posts" key="1">Content of tab 1</TabPane>
+                <TabPane tab="Posts" key="1">
+                    {this.state.loadingGeoLocation ? <Spin tip = "Loading location" indicator={antIcon}  /> : null}
+                </TabPane>
                 <TabPane tab="Map" key="2">Content of tab 2</TabPane>
             </Tabs>
         )
